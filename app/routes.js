56ecd8d89330b3188b4c48d929cd3b4c24b20bca
raparicio6 <rodrigoaparicio6@gmail.com>
2018-09-07 16:40:54 -0300

    Move business logic to model

--- app/routes.js
+++ app/routes.js
@@ -1,9 +1,4 @@
 const models = require('./models');
-const bcrypt = require('../node_modules/bcryptjs');
-
-const saltRounds = 10;
-const validEmailPattern = /^[a-zA-Z0-9_.+-]+@wolox\.com\.ar$/g;
-const validPasswordPattern = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/g;
 
 exports.init = app => {
   app.use('*', function(req, res, next) {
@@ -20,29 +15,23 @@ exports.init = app => {
   app.post(
     '/users',
     function(req, res, next) {
-      if (!req.body.firstName || !req.body.lastName || !req.body.email || !req.body.password)
-        return res.status(400).send('Missing required parameters');
+      if (!models.User.obligatoryParametersWereReceived(Object.keys(req.body)))
+        return res.status(400).send('Missing obligatory parameters');
       next();
     },
     function(req, res, next) {
-      const email = req.body.email;
-      if (!email.match(validEmailPattern)) return res.status(400).send('Invalid email');
+      if (!models.User.emailIsValid(req.body.email)) return res.status(400).send('Invalid email');
       next();
     },
     function(req, res, next) {
-      const password = req.body.password;
-      if (!password.match(validPasswordPattern)) return res.status(400).send('Invalid password');
+      if (!models.User.passwordIsValid(req.body.password)) return res.status(400).send('Invalid password');
       next();
     },
     function(req, res, next) {
-      models.User.findOne({
-        where: {
-          email: req.body.email
-        }
-      })
-        .then(user => {
-          if (user) return res.status(400).send('Email is already in use');
-          next();
+      models.User.emailIsAvailable(req.body.email)
+        .then(isAvailable => {
+          if (isAvailable) return next();
+          return res.status(400).send('Email is already in use');
         })
         .catch(error => {
           console.log(error.message);
@@ -50,22 +39,20 @@ exports.init = app => {
         });
     },
     function(req, res, next) {
-      bcrypt.hash(req.body.password, saltRounds).then(hash => {
-        models.User.create({
-          firstName: req.body.firstName,
-          lastName: req.body.lastName,
-          email: req.body.email,
-          password: hash
+      models.User.create({
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        password: req.body.password
+      })
+        .then(user => {
+          user.printAfterCreationMessage();
+          next();
         })
-          .then(user => {
-            console.log(`${user.firstName}´s user has been created`);
-            next();
-          })
-          .catch(error => {
-            console.log(error.message);
-            return res.status(503).send(error.message);
-          });
-      });
+        .catch(error => {
+          console.log(error.message);
+          return res.status(503).send(error.message);
+        });
     },
     function(req, res) {
       return res.send(200);
