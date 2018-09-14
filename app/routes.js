870e81993d4bcf4a3ba725a7762df265eb9f7f62
raparicio6 <rodrigoaparicio6@gmail.com>
2018-09-14 18:06:39 -0300

    Sign in method together with its validations finished

--- app/routes.js
+++ app/routes.js
@@ -2,4 +2,6 @@ const userController = require('./controllers/users');
 
 exports.init = app => {
   app.post('/users', userController.createUser);
+
+  app.post('/users/sessions', userController.logIn);
 };
