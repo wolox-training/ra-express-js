3ead52bc9250311a9d3bf71a470cb355538aea77
raparicio6 <rodrigoaparicio6@gmail.com>
2018-09-19 12:27:47 -0300

    Rebasing with Sign-Up-tests-documentation

--- test/users.js
+++ test/users.js
@@ -89,7 +89,7 @@ describe('/users POST', () => {
         err.response.should.be.json;
         err.response.body.should.have.property('message');
         err.response.body.should.have.property('internal_code');
-        chai.expect(err.response.body.internal_code).to.equal('invalid_user_parameters');
+        chai.expect(err.response.body.internal_code).to.equal('invalid_parameters');
       });
   });
 
@@ -107,7 +107,7 @@ describe('/users POST', () => {
         err.response.should.be.json;
         err.response.body.should.have.property('message');
         err.response.body.should.have.property('internal_code');
-        chai.expect(err.response.body.internal_code).to.equal('invalid_user_parameters');
+        chai.expect(err.response.body.internal_code).to.equal('invalid_parameters');
       });
   });
 
@@ -125,7 +125,7 @@ describe('/users POST', () => {
         err.response.should.be.json;
         err.response.body.should.have.property('message');
         err.response.body.should.have.property('internal_code');
-        chai.expect(err.response.body.internal_code).to.equal('invalid_user_parameters');
+        chai.expect(err.response.body.internal_code).to.equal('invalid_parameters');
       });
   });
 
@@ -143,7 +143,7 @@ describe('/users POST', () => {
         err.response.should.be.json;
         err.response.body.should.have.property('message');
         err.response.body.should.have.property('internal_code');
-        chai.expect(err.response.body.internal_code).to.equal('invalid_user_parameters');
+        chai.expect(err.response.body.internal_code).to.equal('invalid_parameters');
       });
   });
 
